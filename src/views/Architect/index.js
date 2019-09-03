5253bf371f01ac1dc46e28f28e2f37e863aec4ee
praffulcarma <prafful@carmatec.in>
2019-09-03 15:56:42 +0530

    first commit

---
+++ src/views/Architect/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Header from '../../components/Header';
+import Homepage from '../../components/Homepage';
+
+class Architect extends Component {
+  render() {
+    return (
+    	<Header />
+      <Homepage />
+    );
+  }
+}
+
+export default Architect;
